1ccd556806e469549c7ff2ec68cdcf2cfd0f86f2
Jose Nunez <jnunez@msts.com>
2020-11-06 22:18:55 -0600

    Initial Release

---
+++ main.js
@@ -0,0 +1,45 @@
+(() => {
+  const moment = require("moment");
+  
+  /**
+   * Returns the next applicable business day for a given candidate day.
+   * If candidateDate is a buisness day then it returns the same provided candidateDate.
+   * @param {moment} candidateDate
+   * @param {moment} holidaysArray
+   * @returns {moment}
+   */
+  function nextBizDay(candidateDate, holidaysArray) {
+    let mCandidateDate = moment(candidateDate);
+    let dow = mCandidateDate.day();
+
+    //FIX WEEKENDS
+    if (dow === 6 || dow === 0) {
+      if (dow === 0)
+        mCandidateDate = mCandidateDate.add(1, 'day');
+      else
+        mCandidateDate = mCandidateDate.add(2, 'day');
+    }
+
+    //REACT TO NO HoliDays array
+    if (holidaysArray === undefined || holidaysArray.length === undefined || holidaysArray.length === 0) {
+      return mCandidateDate;
+    }
+
+    //SCAN the HolyDays array and use recursion
+    for (var i = 0; i < holidaysArray.length; i++) {
+      let item = holidaysArray[i];
+      const mHoliday = moment(item);
+
+      if (mHoliday.isSameOrAfter(mCandidateDate)) {
+        if (mCandidateDate.isSame(mHoliday)) {
+          let remainingHolidays = holidaysArray.slice(i + 1);
+          return nextBizDay(mCandidateDate.add(1, 'day'), remainingHolidays);
+        }
+      }
+    }
+
+    return mCandidateDate;
+  }
+
+  exports.NextBizDay = nextBizDay;
+})();
---
+++ test.js
@@ -0,0 +1,71 @@
+const moment = require("moment");
+const nbd = require("./main");
+
+function test() {
+  console.log("--------------------------------------------");
+  console.log("--------------------------------------------");
+  console.log("--------------------------------------------");
+
+  const targetCases = [
+    "2020-12-20",
+    "2020-12-21",
+    "2020-12-22",
+    "2020-12-23",
+    "2020-12-24",
+    "2020-12-25",
+    "2020-12-26",
+    "2020-12-27",
+    "2020-12-28",
+    "2020-12-29",
+    "2020-12-30",
+    "2020-12-31",
+    "2021-01-01",
+    "2021-01-02",
+    "2021-01-03",
+    "2021-01-04",
+    "2021-01-05"
+  ];
+  
+  const expectedResults = [
+    "2020-12-21",
+    "2020-12-21",
+    "2020-12-22",
+    "2020-12-23",
+    "2020-12-24",
+    "2020-12-29",
+    "2020-12-29",
+    "2020-12-29",
+    "2020-12-29",
+    "2020-12-29",
+    "2020-12-30",
+    "2020-12-31",
+    "2021-01-05",
+    "2021-01-05",
+    "2021-01-05",
+    "2021-01-05",
+    "2021-01-05"
+  ];  
+
+  const holidaysArray = [
+    "2020-12-25",
+    "2020-12-28",
+    "2021-01-01",
+    "2021-01-04"
+  ];
+
+  console.log("TEST:", {targetCases, holidaysArray});
+
+  let results = [];
+
+  targetCases.forEach((item, i) => {
+    let tDate = moment(item);
+    let rDate = nbd.NextBizDay(tDate, holidaysArray);
+    if(rDate.isSame(expectedResults[i])){
+      console.log(tDate, rDate, "OK");
+    } else {
+      console.error("FAIL");
+    }
+    results.push({tDate, rDate});
+  });
+}
+test();
