33953e9a5d993a5abaea52f38cb868d95d6fe293
Jose Nunez <jnunez@msts.com>
2020-11-07 17:05:09 -0600

    example in Readme

--- example-use.js
+++ example-use.js
@@ -1,19 +1,19 @@
-const moment = require("moment");
 const nbd = require("../next-biz-date");
 
 const holidays = [
-    "2020-12-17",
-    "2020-12-25",
-    "2020-12-28",
-    "2021-01-01",
-    "2021-01-04"
+  "2020-12-17",
+  "2020-12-25",
+  "2020-12-28",
+  "2021-01-01",
+  "2021-01-04"
 ];
 
-const initialDate = "2020-12-24";
-const offset = 1;
-const direction = "FORWARD";
+const testCases = [
+  {initialDate: "2020-12-24", offset: 1, direction: "FORWARD", expected_result: "2020-12-29"},
+  {initialDate: "2020-12-29", offset: 1, direction: "BACKWARDS", expected_result: "2020-12-24"}
+];
 
-const result = nbd.FindNextBizDate(initialDate, holidays, offset, direction);
-
-console.log("RESULT: ", result);
-console.log("INPUTS:",{initialDate, offset, direction});
+testCases.forEach((item, idx)=>{
+  console.log(`CASE #${idx}: `,item);
+  console.log("  RESULT: ", nbd.FindNextBizDate(item.initialDate, holidays, item.offset, item.direction));
+});
