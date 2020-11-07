bb72042d305a6bd5ecd6054ec1a603aad9304f96
Jose Nunez <jose.a.nunez@gmail.com>
2020-11-07 10:36:13 -0600

    Adding offset and direction. Pending test cases for direction backwards

--- main.js
+++ main.js
@@ -1,45 +1,83 @@
 (() => {
-  const moment = require("moment");
-  
-  /**
-   * Returns the next applicable business day for a given candidate day.
-   * If candidateDate is a buisness day then it returns the same provided candidateDate.
-   * @param {moment} candidateDate
-   * @param {moment} holidaysArray
-   * @returns {moment}
-   */
-  function findNextBizDate(candidateDate, holidaysArray) {
-    let mCandidateDate = moment(candidateDate);
-    let dow = mCandidateDate.day();
+    const moment = require("moment");
 
-    //FIX WEEKENDS
-    if (dow === 6 || dow === 0) {
-      if (dow === 0)
-        mCandidateDate = mCandidateDate.add(1, 'day');
-      else
-        mCandidateDate = mCandidateDate.add(2, 'day');
-    }
-
-    //REACT TO NO HoliDays array
-    if (holidaysArray === undefined || holidaysArray.length === undefined || holidaysArray.length === 0) {
-      return mCandidateDate;
-    }
-
-    //SCAN the HolyDays array and use recursion
-    for (var i = 0; i < holidaysArray.length; i++) {
-      let item = holidaysArray[i];
-      const mHoliday = moment(item);
-
-      if (mHoliday.isSameOrAfter(mCandidateDate)) {
-        if (mCandidateDate.isSame(mHoliday)) {
-          let remainingHolidays = holidaysArray.slice(i + 1);
-          return findNextBizDate(mCandidateDate.add(1, 'day'), remainingHolidays);
+    function findNextBizDate(baseDate, holidaysArray, offset, direction) {
+        if (!offset) {
+            offset = 0;
         }
-      }
+
+        if (!direction) {
+            direction = "FORWARD";
+        }
+
+        if (direction === "FORWARD") {
+            let targetDate = moment(baseDate).add(offset, 'days');
+            return findBizDate(targetDate, holidaysArray);
+        } else {
+            let targetDate = moment(baseDate).add(-1 * offset, 'days');
+            return findBizDate(targetDate, holidaysArray);
+        }
     }
 
-    return mCandidateDate;
-  }
+    /**
+     * Returns the next applicable business day for a given candidate day.
+     * If candidateDate is a buisness day then it returns the same provided candidateDate.
+     * @param {moment} candidateDate Date to evaluate first for being bizdate (moment or string on yyyy-mm-dd format)
+     * @param {moment} holidaysArray Array of holidays (strings on yyyy-mm-dd format)
+     * @param {string} direction FORWARD or BACKWARDS
+     * @returns {moment}
+     */
+    function findBizDate(candidateDate, holidaysArray, direction) {
+        let mCandidateDate = moment(candidateDate);
+        let dow = mCandidateDate.day();
+        if (!direction) {
+            direction = "FORWARD";
+        }
 
-  exports.FindNextBizDate = findNextBizDate;
+        //FIX WEEKENDS
+        if (dow === 6 || dow === 0) {
+            if (dow === 0)
+                mCandidateDate = mCandidateDate.add(1, 'day');
+            else
+                mCandidateDate = mCandidateDate.add(2, 'day');
+        }
+
+        //REACT TO NO HoliDays array
+        if (holidaysArray === undefined || holidaysArray.length === undefined || holidaysArray.length === 0) {
+            return mCandidateDate;
+        }
+
+        //SCAN the HolyDays array and use recursion
+        let initial, increment, exitCondition;
+        if (direction === "FORWARD") {
+            initial = 0;
+            increment = 1;
+            exitCondition = (value) => {
+                return value < holidaysArray.length;
+            };
+        } else {
+            initial = holidaysArray.length - 1;
+            increment = -1;
+            exitCondition = (value) => {
+                return value >= 0;
+            };
+        }
+
+        for (var i = initial; exitCondition(i); i += increment) {
+            let item = holidaysArray[i];
+            const mHoliday = moment(item);
+
+            if (mHoliday.isSameOrAfter(mCandidateDate)) {
+                if (mCandidateDate.isSame(mHoliday)) {
+                    let remainingHolidays = holidaysArray.slice(i + 1);
+                    return findBizDate(mCandidateDate.add(1, 'day'), remainingHolidays);
+                }
+            }
+        }
+
+        return mCandidateDate;
+    }
+
+    exports.FindBizDate = findBizDate;
+    exports.FindNextBizDate = findNextBizDate;
 })();
--- test/basic.test.js
+++ test/basic.test.js
@@ -2,66 +2,84 @@ const assert = require("assert");
 const moment = require("moment");
 const nbd = require("../main");
 
-describe("Base Test Scenario", () => {
-  it("Should properly do happy-path cases", () => {
-    const happyPathCases = [
-      "2020-12-20",
-      "2020-12-21",
-      "2020-12-22",
-      "2020-12-23",
-      "2020-12-24",
-      "2020-12-25",
-      "2020-12-26",
-      "2020-12-27",
-      "2020-12-28",
-      "2020-12-29",
-      "2020-12-30",
-      "2020-12-31",
-      "2021-01-01",
-      "2021-01-02",
-      "2021-01-03",
-      "2021-01-04",
-      "2021-01-05"
-    ];
+const holidays = [
+    "2020-12-25",
+    "2020-12-28",
+    "2021-01-01",
+    "2021-01-04"
+];
 
-    const expectedResults = [
-      "2020-12-21",
-      "2020-12-21",
-      "2020-12-22",
-      "2020-12-23",
-      "2020-12-24",
-      "2020-12-29",
-      "2020-12-29",
-      "2020-12-29",
-      "2020-12-29",
-      "2020-12-29",
-      "2020-12-30",
-      "2020-12-31",
-      "2021-01-05",
-      "2021-01-05",
-      "2021-01-05",
-      "2021-01-05",
-      "2021-01-05"
-    ];
 
-    const holidays = [
-      "2020-12-25",
-      "2020-12-28",
-      "2021-01-01",
-      "2021-01-04"
-    ];
+describe("FindBizDate", () => {
+    it("Should Find Biz Date based on CandidateDate", () => {
+        const happyPathCases = [
+            "2020-12-20",
+            "2020-12-21",
+            "2020-12-22",
+            "2020-12-23",
+            "2020-12-24",
+            "2020-12-25",
+            "2020-12-26",
+            "2020-12-27",
+            "2020-12-28",
+            "2020-12-29",
+            "2020-12-30",
+            "2020-12-31",
+            "2021-01-01",
+            "2021-01-02",
+            "2021-01-03",
+            "2021-01-04",
+            "2021-01-05"
+        ];
 
-    console.log("TEST:", {happyPathCases, holidays, expectedResults});
+        const expectedResults = [
+            "2020-12-21",
+            "2020-12-21",
+            "2020-12-22",
+            "2020-12-23",
+            "2020-12-24",
+            "2020-12-29",
+            "2020-12-29",
+            "2020-12-29",
+            "2020-12-29",
+            "2020-12-29",
+            "2020-12-30",
+            "2020-12-31",
+            "2021-01-05",
+            "2021-01-05",
+            "2021-01-05",
+            "2021-01-05",
+            "2021-01-05"
+        ];
 
-    let results = [];
+        console.log("TEST:", {happyPathCases, holidays, expectedResults});
 
-    happyPathCases.forEach((item, i) => {
-      let tDate = moment(item);
-      let rDate = nbd.FindNextBizDate(tDate, holidays);
+        let results = [];
 
-      console.log("Testing", tDate, expectedResults[i], rDate);
-      assert.equal(rDate.isSame(expectedResults[i]), true);
-      results.push({tDate, rDate});
+        happyPathCases.forEach((item, i) => {
+            let tDate = moment(item);
+            let rDate = nbd.FindBizDate(tDate, holidays);
+
+            console.log("Testing", tDate, expectedResults[i], rDate);
+            assert.equal(rDate.isSame(expectedResults[i]), true);
+            results.push({tDate, rDate});
+        });
     });
-  });
+});
+
+describe("FindNextBizDate", () => {
+    it("Should be able to handle offset forward cases", () => {
+        let forwardCases = [
+            {initialDate: "2020-12-19", offset: 0, expectedDate: "2020-12-21"},
+            {initialDate: "2020-12-19", offset: 1, expectedDate: "2020-12-21"},
+            {initialDate: "2020-12-19", offset: 2, expectedDate: "2020-12-21"},
+            {initialDate: "2020-12-19", offset: 3, expectedDate: "2020-12-22"}
+        ];
+
+        forwardCases.forEach((caseItem, i) => {
+            let result = nbd.FindNextBizDate(caseItem.initialDate, holidays, caseItem.offset, "FORWARD");
+            console.log("RESULT", moment(caseItem.initialDate), caseItem.offset, moment(caseItem.expectedDate), result);
+            assert.equal(result.isSame(caseItem.expectedDate), true);
+        });
+    });
 });
