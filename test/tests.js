8f7a6c886cd82a375f578e26ff33c14cff2aa4e5
Jose Nunez <jnunez@msts.com>
2020-11-10 18:05:03 -0600

    Improved test case comments

--- test/tests.js
+++ test/tests.js
@@ -50,19 +50,20 @@ describe("findNextBizDate(baseDate, holidaysArray, offset, direction)", () => {
   it("Should be able to handle offset COUNTING forward cases", () => {
     let forwardCases = [
       {baseDate: "2020-12-17", offset: 0 /*  0                     */, expectedResult: "2020-12-17"},
-      {baseDate: "2020-12-17", offset: 1 /*  0>>>1                 */, expectedResult: "2020-12-21"},
+      {baseDate: "2020-12-17", offset: 1 /*  0HWW1                 */, expectedResult: "2020-12-21"},
 
-      {baseDate: "2020-12-19", offset: 0 /*  >>0                   */, expectedResult: "2020-12-21"},
-      {baseDate: "2020-12-19", offset: 1 /*  >>1                   */, expectedResult: "2020-12-21"},
-      {baseDate: "2020-12-19", offset: 2 /*  >>12                  */, expectedResult: "2020-12-22"},
-      {baseDate: "2020-12-19", offset: 3 /*  >>123                 */, expectedResult: "2020-12-23"},
-      {baseDate: "2020-12-19", offset: 4 /*  >>1234                */, expectedResult: "2020-12-24"},
-      {baseDate: "2020-12-19", offset: 5 /*  >>1234>>>>5           */, expectedResult: "2020-12-29"},
-      {baseDate: "2020-12-19", offset: 6 /*  >>1234>>>>56          */, expectedResult: "2020-12-30"},
-      {baseDate: "2020-12-19", offset: 7 /*  >>1234>>>>567         */, expectedResult: "2020-12-31"},
-      {baseDate: "2020-12-19", offset: 8 /*  >>1234>>>>567>>>>8    */, expectedResult: "2021-01-05"},
-      {baseDate: "2020-12-19", offset: 9 /*  >>1234>>>>567>>>>89   */, expectedResult: "2021-01-06"},
-      {baseDate: "2020-12-19", offset: 10 /* >>1234>>>>567>>>>89A  */, expectedResult: "2021-01-07"}
+      {baseDate: "2020-12-19", offset: 0 /*  WW0                   */, expectedResult: "2020-12-21"},
+      {baseDate: "2020-12-19", offset: 1 /*  WW1                   */, expectedResult: "2020-12-21"},
+      {baseDate: "2020-12-19", offset: 2 /*  WW12                  */, expectedResult: "2020-12-22"},
+      {baseDate: "2020-12-19", offset: 3 /*  WW123                 */, expectedResult: "2020-12-23"},
+      {baseDate: "2020-12-19", offset: 4 /*  WW1234                */, expectedResult: "2020-12-24"},
+      {baseDate: "2020-12-19", offset: 5 /*  WW1234HWWH5           */, expectedResult: "2020-12-29"},
+      {baseDate: "2020-12-19", offset: 6 /*  WW1234HWWH56          */, expectedResult: "2020-12-30"},
+      {baseDate: "2020-12-19", offset: 7 /*  WW1234HWWH567         */, expectedResult: "2020-12-31"},
+      {baseDate: "2020-12-19", offset: 8 /*  WW1234HWWH567HWWH8    */, expectedResult: "2021-01-05"},
+      {baseDate: "2020-12-19", offset: 9 /*  WW1234HWWH567HWWH89   */, expectedResult: "2021-01-06"},
+      {baseDate: "2020-12-19", offset: 10 /* WW1234HWWH567HWWH890  */, expectedResult: "2021-01-07"},
+      {baseDate: "2020-12-19", offset: 11 /* WW1234HWWH567HWWH8901 */, expectedResult: "2021-01-08"},
     ];
 
     let results = [];
@@ -81,24 +82,24 @@ describe("findNextBizDate(baseDate, holidaysArray, offset, direction)", () => {
     let backwardsCases = [
       {baseDate: "2020-12-30", offset: 0 /*                      0  */, expectedResult: "2020-12-30"},
       {baseDate: "2020-12-30", offset: 1 /*                     10  */, expectedResult: "2020-12-29"},
-      {baseDate: "2020-12-30", offset: 2 /*                2<<<<10  */, expectedResult: "2020-12-24"},
-      {baseDate: "2020-12-30", offset: 3 /*               32<<<<10  */, expectedResult: "2020-12-23"},
-      {baseDate: "2020-12-30", offset: 4 /*              432<<<<10  */, expectedResult: "2020-12-22"},
-      {baseDate: "2020-12-30", offset: 5 /*             5432<<<<10  */, expectedResult: "2020-12-21"},
-      {baseDate: "2020-12-30", offset: 6 /*         6<<<5432<<<<10  */, expectedResult: "2020-12-17"},
+      {baseDate: "2020-12-30", offset: 2 /*                2HWWH10  */, expectedResult: "2020-12-24"},
+      {baseDate: "2020-12-30", offset: 3 /*               32HWWH10  */, expectedResult: "2020-12-23"},
+      {baseDate: "2020-12-30", offset: 4 /*              432HWWH10  */, expectedResult: "2020-12-22"},
+      {baseDate: "2020-12-30", offset: 5 /*             5432HWWH10  */, expectedResult: "2020-12-21"},
+      {baseDate: "2020-12-30", offset: 6 /*         6HWW5432HWWH10  */, expectedResult: "2020-12-17"},
 
-      {baseDate: "2021-01-07", offset: 7 /*       76<<<<543<<<<210  */, expectedResult: "2020-12-23"},
-      {baseDate: "2021-01-07", offset: 8 /*      876<<<<543<<<<210  */, expectedResult: "2020-12-22"},
-      {baseDate: "2021-01-07", offset: 9 /*     9876<<<<543<<<<210  */, expectedResult: "2020-12-21"},
+      {baseDate: "2021-01-07", offset: 7 /*       76HWWH543HWWH210  */, expectedResult: "2020-12-23"},
+      {baseDate: "2021-01-07", offset: 8 /*      876HWWH543HWWH210  */, expectedResult: "2020-12-22"},
+      {baseDate: "2021-01-07", offset: 9 /*     9876HWWH543HWWH210  */, expectedResult: "2020-12-21"},
 
-      {baseDate: "2021-01-03", offset: 8 /*     8<<<7654<<<<321<<<  */, expectedResult: "2020-12-17"},
+      {baseDate: "2021-01-03", offset: 8 /*     8HWW7654HWWH321HWW  */, expectedResult: "2020-12-17"},
 
-      {baseDate: "2020-12-27", offset: 0 /*                   0<<<  */, expectedResult: "2020-12-24"},
-      {baseDate: "2020-12-27", offset: 1 /*                   1<<<  */, expectedResult: "2020-12-24"},
-      {baseDate: "2020-12-27", offset: 2 /*                  21<<<  */, expectedResult: "2020-12-23"},
-      {baseDate: "2020-12-27", offset: 3 /*                 321<<<  */, expectedResult: "2020-12-22"},
-      {baseDate: "2020-12-27", offset: 4 /*                4321<<<  */, expectedResult: "2020-12-21"},
-      {baseDate: "2020-12-27", offset: 5 /*            5<<<4321<<<  */, expectedResult: "2020-12-17"}
+      {baseDate: "2020-12-27", offset: 0 /*                   0HWW  */, expectedResult: "2020-12-24"},
+      {baseDate: "2020-12-27", offset: 1 /*                   1HWW  */, expectedResult: "2020-12-24"},
+      {baseDate: "2020-12-27", offset: 2 /*                  21HWW  */, expectedResult: "2020-12-23"},
+      {baseDate: "2020-12-27", offset: 3 /*                 321HWW  */, expectedResult: "2020-12-22"},
+      {baseDate: "2020-12-27", offset: 4 /*                4321HWW  */, expectedResult: "2020-12-21"},
+      {baseDate: "2020-12-27", offset: 5 /*            5HWW4321HWW  */, expectedResult: "2020-12-17"}
     ];
 
     let results = [];
